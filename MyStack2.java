1fc760bf9c949da124c92dc1bcc8f01ab0e29e0f
damonxyf <34394210+damonxyf@users.noreply.github.com>
2017-12-10 18:17:21 +0800

    Add files via upload

---
+++ MyStack2.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class MyStack2 {
+    private Stack<Integer> stackData;
+    private Stack<Integer> stackMin;
+
+    public MyStack2(){
+        this.stackData = new Stack<Integer>();
+        this.stackMin = new Stack<Integer>();
+    }
+
+    public void push(int newNum){
+        if(this.stackData.isEmpty()){
+            this.stackMin.push(newNum);
+        }else if (newNum < this.getmin()){
+            this.stackMin.push(newNum);
+        }else{
+            int newMin = this.stackMin.peek();
+            this.stackData.push(newMin);
+        }
+        this.stackData.push(newNum);
+    }
+
+    public int pop(){
+        if(this.stackData.isEmpty()){
+            throw new RuntimeException("Your Stack is empty");
+        }
+        this.stackMin.pop();
+        return this.stackMin.pop();
+    }
+
+    public int getmin(){
+        if (this.stackMin.isEmpty()){
+            throw new RuntimeException("Your Stack is empty");
+        }
+        return this.stackMin.peek();
+    }
+
+}
